b696ff620bdfeaa2cbd10e7de8c8865baa39cd96
Ema Martins <150731688+ema-12-martins@users.noreply.github.com>
2025-11-05 17:09:37 +0000

    Menu Dificuldade - Difficuldade no GAMEDATA

---
+++ Races/Assets/Scripts/DefineDifficulty.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DefineDifficulty : MonoBehaviour
+{
+    public void SetDifficulty(int level)
+    {
+        if (level == 1)
+        {
+            GameData.probabilityOfOvercomingObstacles = 0.3f;
+        }else if (level == 2)
+        {
+            GameData.probabilityOfOvercomingObstacles = 0.6f;
+        }
+        else if (level == 3)
+        {
+            GameData.probabilityOfOvercomingObstacles = 0.9f;
+        }
+    }
+
+}
--- Races/Assets/Scripts/GameData.cs
+++ Races/Assets/Scripts/GameData.cs
@@ -4,4 +4,5 @@ public static class GameData
 {
     public static GameObject selectedCarPrefab = null;
     public static string finalText = null; //If win or lost
+    public static float probabilityOfOvercomingObstacles = 0;
 }
